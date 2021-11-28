b0d189efdb42b0a05f01ec1e66000f4ea097e5e6
apasasm <31715137+apasasm@users.noreply.github.com>
2021-11-29 08:58:38 +0900

    fix

--- src/background.js
+++ src/background.js
@@ -20,7 +20,8 @@ function changeBackgroundImg() {
   // htmlBody.style.backgroundImage = `../img/${selectedImg}`;
 
   // document.body.style.backgroundImage = `url('../img/${selectedImg}')`;
-  document.body.style.backgroundImage = `url(https://github.com/apasasm/curly-umbrella.github.io/raw/main/img/${selectedImg})`;
+  // document.body.style.backgroundImage = `url(https://github.com/apasasm/curly-umbrella.github.io/raw/main/img/`;
+  document.body.style.backgroundImage = `url(https://github.com/apasasm/curly-umbrella.github.io/blob/main/img/${selectedImg})?raw=true`;
   document.body.style.backgroundRepeat = "no-repeat";
   document.body.style.backgroundSize = "cover";
 }
