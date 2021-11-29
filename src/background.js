87efe35c2f9887e544b6e3a6063625b76ab98edb
apasasm <31715137+apasasm@users.noreply.github.com>
2021-11-29 18:32:24 +0900

    add background image

--- src/background.js
+++ src/background.js
@@ -1,4 +1,4 @@
-const baclgroundImg = ["img1.jpg", "img2.jpg"];
+const baclgroundImg = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg"];
 
 function getRandomIndex(object) {
   const selectedIndex = Math.floor(Math.random() * object.length);
@@ -7,12 +7,9 @@ function getRandomIndex(object) {
 
 function changeBackgroundImg() {
   const selectedImg = baclgroundImg[getRandomIndex(baclgroundImg)];
-  // document.body.style.backgroundImage = `url("img1.jpg")`;
-  // document.body.style.backgroundImage = `url("/src/img/img1.jpg")`;
   document.body.style.backgroundImage = `url("${selectedImg}")`;
   document.body.style.backgroundRepeat = "no-repeat";
   document.body.style.backgroundSize = "cover";
-  console.log(document.body.style.backgroundImage);
 }
 
 const htmlBody = document.querySelector("body");
