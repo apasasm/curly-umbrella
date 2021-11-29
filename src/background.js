4a17aca004c9db6663f18297f2b6e0e9b20ce1f2
apasasm <31715137+apasasm@users.noreply.github.com>
2021-11-29 09:35:51 +0900

    fix

--- src/background.js
+++ src/background.js
@@ -9,11 +9,13 @@ function changeBackgroundImg() {
   const selectedImg = baclgroundImg[getRandomIndex(baclgroundImg)];
   // htmlBody.style.backgroundImage = `../img/${selectedImg}`;
 
-  document.body.style.backgroundImage = `url('/img/${selectedImg}')`;
+  // document.body.style.backgroundImage = `url('img/${selectedImg}')`;
+  // document.body.style.backgroundImage =
+  //   "url('/Users/nano/Temp/gitmy/curly-umbrella.github.io/img1.jpg')";
+  // document.body.style.backgroundImage = `/Users/nano/Temp/gitmy/curly-umbrella.github.io/img1.jpg'`;
   console.log(document.body.style.backgroundImage);
-  // document.body.style.backgroundImage = `url(https://github.com/apasasm/curly-umbrella.github.io/raw/main/img/`;
+  document.body.style.backgroundImage = `url("img1.jpg")`;
   // document.body.style.backgroundImage = `url(https://github.com/apasasm/curly-umbrella.github.io/blob/main/img/${selectedImg})?raw=true`;
-  // document.body.style.backgroundImage = `url("https://lawrencecheng123.github.io/img/${selectedImg})`;
   document.body.style.backgroundRepeat = "no-repeat";
   document.body.style.backgroundSize = "cover";
 }
