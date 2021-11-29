1669403348e132cdc30caac39101da07e38a2d9a
apasasm <31715137+apasasm@users.noreply.github.com>
2021-11-29 09:41:40 +0900

    image path test

--- src/background.js
+++ src/background.js
@@ -7,17 +7,11 @@ function getRandomIndex(object) {
 
 function changeBackgroundImg() {
   const selectedImg = baclgroundImg[getRandomIndex(baclgroundImg)];
-  // htmlBody.style.backgroundImage = `../img/${selectedImg}`;
-
-  // document.body.style.backgroundImage = `url('img/${selectedImg}')`;
-  // document.body.style.backgroundImage =
-  //   "url('/Users/nano/Temp/gitmy/curly-umbrella.github.io/img1.jpg')";
-  // document.body.style.backgroundImage = `/Users/nano/Temp/gitmy/curly-umbrella.github.io/img1.jpg'`;
-  document.body.style.backgroundImage = `url("img1.jpg")`;
-  console.log(document.body.style.backgroundImage);
-  // document.body.style.backgroundImage = `url(https://github.com/apasasm/curly-umbrella.github.io/blob/main/img/${selectedImg})?raw=true`;
+  // document.body.style.backgroundImage = `url("img1.jpg")`;
+  document.body.style.backgroundImage = `url("/src/img/img1.jpg")`;
   document.body.style.backgroundRepeat = "no-repeat";
   document.body.style.backgroundSize = "cover";
+  console.log(document.body.style.backgroundImage);
 }
 
 const htmlBody = document.querySelector("body");
