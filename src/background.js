93fb43d13e08a594bf99030998c5e4f417dc726b
apasasm <31715137+apasasm@users.noreply.github.com>
2021-11-29 09:07:11 +0900

    fix

--- src/background.js
+++ src/background.js
@@ -1,14 +1,4 @@
-const baclgroundImg = [
-  "img1.jpg",
-  "img2.jpg",
-  "img3.jpg",
-  "img4.jpg",
-  "img5.jpg",
-  "img6.jpg",
-  "img7.jpg",
-  "img8.jpg",
-  "img9.jpg",
-];
+const baclgroundImg = ["img1.jpg", "img2.jpg"];
 
 function getRandomIndex(object) {
   const selectedIndex = Math.floor(Math.random() * object.length);
@@ -19,7 +9,8 @@ function changeBackgroundImg() {
   const selectedImg = baclgroundImg[getRandomIndex(baclgroundImg)];
   // htmlBody.style.backgroundImage = `../img/${selectedImg}`;
 
-  document.body.style.backgroundImage = `url('../img/${selectedImg}')`;
+  document.body.style.backgroundImage = `url('/img/${selectedImg}')`;
+  console.log(document.body.style.backgroundImage);
   // document.body.style.backgroundImage = `url(https://github.com/apasasm/curly-umbrella.github.io/raw/main/img/`;
   // document.body.style.backgroundImage = `url(https://github.com/apasasm/curly-umbrella.github.io/blob/main/img/${selectedImg})?raw=true`;
   // document.body.style.backgroundImage = `url("https://lawrencecheng123.github.io/img/${selectedImg})`;
